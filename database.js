4c7aaeaa2787cc41abf0377e19f4456787f7dbe0
FreitasProcopio <pp918545@gmail.com>
2025-05-09 16:51:41 -0300

    sparkles: feat: Add sprint five

---
+++ database.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+import 'dotenv/config';
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'sqlite',
+    storage: './src/data/base.db', // Especifica o arquivo SQLite
+  }
+);
+// Teste a conexão
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
